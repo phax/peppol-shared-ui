fc681c6fec9ff0c03fd477dbf2191c239661e40f
Philip Helger <philip@helger.com>
2025-11-18 16:57:40 +0100

    Ctd

--- peppol-ui/src/main/java/com/helger/peppol/ui/PeppolUI.java
+++ peppol-ui/src/main/java/com/helger/peppol/ui/PeppolUI.java
@@ -19,8 +19,6 @@ package com.helger.peppol.ui;
 import java.util.List;
 
 import org.jspecify.annotations.NonNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.helger.annotation.Nonempty;
 import com.helger.annotation.concurrent.Immutable;
@@ -31,6 +29,9 @@ import com.helger.base.string.StringHelper;
 import com.helger.base.type.ITypedObject;
 import com.helger.collection.commons.CommonsArrayList;
 import com.helger.collection.commons.ICommonsMap;
+import com.helger.html.hc.html.grouping.HCUL;
+import com.helger.html.hc.html.textlevel.HCCode;
+import com.helger.peppolid.peppol.doctype.IPeppolDocumentTypeIdentifierParts;
 import com.helger.photon.uicore.css.CPageParam;
 import com.helger.photon.uicore.page.IWebPageExecutionContext;
 import com.helger.url.ISimpleURL;
@@ -46,8 +47,6 @@ public final class PeppolUI
   public static final ICommonsMap <String, String> ROLE_CONFIG_CUSTOMATTRS = null;
   @CodingStyleguideUnaware
   public static final List <String> REQUIRED_ROLE_IDS_CONFIG = new CommonsArrayList <> (ROLE_CONFIG_ID).getAsUnmodifiable ();
-
-  private static final Logger LOGGER = LoggerFactory.getLogger (PeppolUI.class);
 
   private PeppolUI ()
   {}
@@ -76,5 +75,16 @@ public final class PeppolUI
     return aWPEC.getLinkToMenuItem (sMenuItemID)
                 .add (CPageParam.PARAM_ACTION, CPageParam.ACTION_VIEW)
                 .add (CPageParam.PARAM_OBJECT, sObjectID);
+  }
+
+  @NonNull
+  public static HCUL getDocumentTypeIDDetails (@NonNull final IPeppolDocumentTypeIdentifierParts aParts)
+  {
+    final HCUL aUL = new HCUL ();
+    aUL.addItem ().addChild ("Root namespace: ").addChild (new HCCode ().addChild (aParts.getRootNS ()));
+    aUL.addItem ().addChild ("Local name: ").addChild (new HCCode ().addChild (aParts.getLocalName ()));
+    aUL.addItem ().addChild ("Customization ID: ").addChild (new HCCode ().addChild (aParts.getCustomizationID ()));
+    aUL.addItem ().addChild ("Version: ").addChild (new HCCode ().addChild (aParts.getVersion ()));
+    return aUL;
   }
 }
