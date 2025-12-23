96c06ee507c92630aaeb7d08314f93028accc0a0
Philip Helger <philip@helger.com>
2025-12-23 16:53:40 +0100

    Beautified error display

--- peppol-shared-ui/src/main/java/com/helger/peppol/sharedui/page/pub/SMPExceptionHandler.java
+++ peppol-shared-ui/src/main/java/com/helger/peppol/sharedui/page/pub/SMPExceptionHandler.java
@@ -8,13 +8,15 @@ import com.helger.base.CGlobal;
 import com.helger.base.callback.exception.IExceptionCallback;
 import com.helger.base.enforce.ValueEnforcer;
 import com.helger.html.hc.IHCNode;
-import com.helger.html.hc.html.grouping.HCDiv;
+import com.helger.html.hc.ext.HCExtHelper;
+import com.helger.html.hc.html.grouping.HCHR;
 import com.helger.http.CHttp;
 import com.helger.peppol.sml.ESMPAPIType;
 import com.helger.photon.bootstrap4.alert.BootstrapErrorBox;
+import com.helger.photon.bootstrap4.traits.IHCBootstrap4Trait;
 import com.helger.smpclient.exception.SMPClientHttpException;
 
-final class SMPExceptionHandler implements IExceptionCallback <Exception>
+final class SMPExceptionHandler implements IExceptionCallback <Exception>, IHCBootstrap4Trait
 {
   @NonNull
   private final ESMPAPIType m_eAPIType;
@@ -45,7 +47,7 @@ final class SMPExceptionHandler implements IExceptionCallback <Exception>
 
         if (nStatusCode == CHttp.HTTP_NOT_FOUND)
         {
-          // This is expected and handled via the UI
+          // This is expected and handled properly via the UI
           nStatusCode = CGlobal.ILLEGAL_UINT;
           bIsError = false;
         }
@@ -54,27 +56,33 @@ final class SMPExceptionHandler implements IExceptionCallback <Exception>
     if (nStatusCode != CGlobal.ILLEGAL_UINT)
     {
       if (m_eAPIType == ESMPAPIType.PEPPOL && nStatusCode >= 300 && nStatusCode < 400)
-        m_aHCNode = new BootstrapErrorBox ().addChild ("The SMP server responsed with an HTTP Redirect Code (" +
-                                                       nStatusCode +
-                                                       ") which is not allowed according to Peppol SMP specification.");
+      {
+        m_aHCNode = error ().addChild ("The SMP server responsed with an HTTP Redirect Code (" +
+                                       nStatusCode +
+                                       ") which is not allowed according to Peppol SMP specification.");
+      }
       else
-        m_aHCNode = new BootstrapErrorBox ().addChild ("The SMP server responded with an HTTP Status Code " +
-                                                       nStatusCode +
-                                                       ". Technical details: " +
-                                                       ex.getMessage ());
+      {
+        m_aHCNode = error ().addChildren (div ("The SMP server responded with an HTTP Status Code " +
+                                               nStatusCode +
+                                               "."),
+                                          new HCHR (),
+                                          HCExtHelper.nl2divNodeList ("Technical details: " + ex.getMessage ()));
+      }
     }
     else
     {
       if (bIsError)
       {
         final String sTechnicalDetails = ex.getMessage ();
-        final BootstrapErrorBox aErrBox = new BootstrapErrorBox ().addChildren (new HCDiv ().addChild ("Technical error occurred while querying the SMP."),
-                                                                          new HCDiv ().addChild ("Technical details: " +
-                                                                                                 sTechnicalDetails));
+        final BootstrapErrorBox aErrBox = error ().addChildren (div ("Technical error occurred while querying the SMP."),
+                                                                new HCHR (),
+                                                                HCExtHelper.nl2divNodeList ("Technical details: " +
+                                                                                            sTechnicalDetails));
         if (bIsDirectoryQuery &&
             sTechnicalDetails.contains ("unable to find valid certification path to requested target"))
         {
-          aErrBox.addChild (new HCDiv ().addChild ("The error most likely stems from an outdated trusted certificate list on my end - don't worry too much."));
+          aErrBox.addChild (div ("The error most likely stems from an outdated trusted certificate list on my end - don't worry too much."));
         }
         m_aHCNode = aErrBox;
       }
