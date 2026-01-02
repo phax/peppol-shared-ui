c66566d7aa8333647cd836ffe0ca8ece97db6e6f
Philip Helger <philip@helger.com>
2026-01-02 13:09:57 +0100

    Added test

---
+++ peppol-shared-api/src/test/java/com/helger/peppol/api/rest/PeppolAPIHelperTest.java
@@ -0,0 +1,40 @@
+package com.helger.peppol.api.rest;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.peppol.businesscard.generic.PDBusinessCard;
+import com.helger.peppol.sml.ESML;
+import com.helger.peppol.sml.ESMPAPIType;
+import com.helger.peppol.ui.types.feedbackcb.FeedbackCallbackLog;
+import com.helger.peppol.ui.types.smp.SMPQueryParams;
+import com.helger.peppolid.factory.PeppolIdentifierFactory;
+import com.helger.peppolid.peppol.PeppolIdentifierHelper;
+
+public class PeppolAPIHelperTest
+{
+
+  private static final Logger LOGGER = LoggerFactory.getLogger (PeppolAPIHelperTest.class);
+
+  @Test
+  public void testReadBCOnfactInvalid ()
+  {
+    final SMPQueryParams aSMPQueryParams = SMPQueryParams.createForSMLOrNull (ESML.DIGIT_PRODUCTION,
+                                                                              ESMPAPIType.PEPPOL,
+                                                                              PeppolIdentifierFactory.INSTANCE,
+                                                                              PeppolIdentifierHelper.DEFAULT_PARTICIPANT_SCHEME,
+                                                                              "9925:be078220767",
+                                                                              true);
+    assertNotNull (aSMPQueryParams);
+    final PDBusinessCard aBC = PeppolAPIHelper.retrieveBusinessCardParsed ("",
+                                                                           aSMPQueryParams,
+                                                                           x -> {},
+                                                                           new FeedbackCallbackLog (LOGGER, ""),
+                                                                           ex -> LOGGER.error ("oops", ex));
+    assertNull (aBC);
+  }
+}
