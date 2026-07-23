801563e2a455b5cc0f7c82c987c4cba89e46d358
Philip Helger <philip@helger.com>
2026-07-23 22:04:55 +0200

    Updated

--- peppol-shared-validation/src/main/java/com/helger/peppol/validate/VESRegistry.java
+++ peppol-shared-validation/src/main/java/com/helger/peppol/validate/VESRegistry.java
@@ -35,85 +35,25 @@ import com.helger.json.JsonArray;
 import com.helger.json.JsonObject;
 import com.helger.phive.api.executorset.IValidationExecutorSet;
 import com.helger.phive.api.executorset.ValidationExecutorSetRegistry;
-import com.helger.phive.cii.CIIValidation;
-import com.helger.phive.ciuspt.CIUS_PTValidation;
-import com.helger.phive.ciusro.CIUS_ROValidation;
-import com.helger.phive.ebinterface.EbInterfaceValidation;
-import com.helger.phive.ehf.EHFValidation;
-import com.helger.phive.en16931.EN16931Validation;
-import com.helger.phive.energieefactuur.EnergieEFactuurValidation;
-import com.helger.phive.eracun.HReRacunValidation;
-import com.helger.phive.facturae.FacturaeValidation;
-import com.helger.phive.fatturapa.FatturaPAValidation;
-import com.helger.phive.finvoice.FinvoiceValidation;
-import com.helger.phive.france.FranceCTCValidation;
-import com.helger.phive.isdoc.ISDOCValidation;
-import com.helger.phive.ksef.KSeFValidation;
-import com.helger.phive.oioubl.OIOUBLValidation;
-import com.helger.phive.osa.OSAValidation;
-import com.helger.phive.peppol.PeppolValidation;
-import com.helger.phive.peppol.italy.PeppolItalyValidation;
 import com.helger.phive.peppol.legacy.PeppolLegacyValidationBisAUNZ;
 import com.helger.phive.peppol.legacy.PeppolLegacyValidationBisEurope;
 import com.helger.phive.peppol.legacy.PeppolLegacyValidationSG;
-import com.helger.phive.serbia.SEOValidation;
-import com.helger.phive.serbia.SRBDTValidation;
-import com.helger.phive.setu.SETUValidation;
-import com.helger.phive.simplerinvoicing.SimplerInvoicingValidation;
-import com.helger.phive.svefaktura.SvefakturaValidation;
-import com.helger.phive.teapps.TEAPPSValidation;
-import com.helger.phive.turkey.TurkeyEFaturaValidation;
-import com.helger.phive.ubl.UBLValidation;
-import com.helger.phive.ublbe.UBLBEValidation;
+import com.helger.phive.rules.all.PhiveRulesValidation;
 import com.helger.phive.xml.source.IValidationSourceXML;
-import com.helger.phive.xrechnung.XRechnungValidation;
-import com.helger.phive.zatca.ZATCAValidation;
-import com.helger.phive.zugferd.ZugferdValidation;
 import com.helger.text.compare.ComparatorHelper;
 
-@SuppressWarnings ("deprecation")
 @Immutable
 public final class VESRegistry
 {
   public static final ValidationExecutorSetRegistry <IValidationSourceXML> VES_REGISTRY = new ValidationExecutorSetRegistry <> ();
   static
   {
-    // Must be first
-    EN16931Validation.initEN16931 (VES_REGISTRY);
-    // The rest in alphabetical order, except where explicitly stated
-    CIIValidation.initCII (VES_REGISTRY);
-    CIUS_PTValidation.initCIUS_PT (VES_REGISTRY);
-    CIUS_ROValidation.initCIUS_RO (VES_REGISTRY);
-    EbInterfaceValidation.initEbInterface (VES_REGISTRY);
-    EHFValidation.initEHF (VES_REGISTRY);
-    FacturaeValidation.initFacturae (VES_REGISTRY);
-    FatturaPAValidation.initFatturaPA (VES_REGISTRY);
-    FinvoiceValidation.initFinvoice (VES_REGISTRY);
-    FranceCTCValidation.initFranceCTC (VES_REGISTRY);
-    HReRacunValidation.init (VES_REGISTRY);
-    KSeFValidation.initKSeF (VES_REGISTRY);
-    ISDOCValidation.initISDOC (VES_REGISTRY);
-    OIOUBLValidation.initOIOUBL (VES_REGISTRY);
-    OSAValidation.initOSA (VES_REGISTRY);
-    PeppolItalyValidation.init (VES_REGISTRY);
-    PeppolValidation.initStandard (VES_REGISTRY);
-    PeppolLegacyValidationBisEurope.init (VES_REGISTRY);
+    PhiveRulesValidation.initPhiveRules (VES_REGISTRY);
+
+    // Legacy
     PeppolLegacyValidationBisAUNZ.init (VES_REGISTRY);
+    PeppolLegacyValidationBisEurope.init (VES_REGISTRY);
     PeppolLegacyValidationSG.init (VES_REGISTRY);
-    SEOValidation.initSEO (VES_REGISTRY);
-    SRBDTValidation.initSRBDT (VES_REGISTRY);
-    SETUValidation.initSETU (VES_REGISTRY);
-    SimplerInvoicingValidation.initSimplerInvoicing (VES_REGISTRY);
-    // Must be after SimplerInvoicing
-    EnergieEFactuurValidation.initEnergieEFactuur (VES_REGISTRY);
-    SvefakturaValidation.initSvefaktura (VES_REGISTRY);
-    TEAPPSValidation.initTEAPPS (VES_REGISTRY);
-    TurkeyEFaturaValidation.initTurkeyEFatura (VES_REGISTRY);
-    UBLValidation.initUBLAllVersions (VES_REGISTRY);
-    UBLBEValidation.initUBLBE (VES_REGISTRY);
-    XRechnungValidation.initXRechnung (VES_REGISTRY);
-    ZATCAValidation.initZATCA (VES_REGISTRY);
-    ZugferdValidation.initZugferd (VES_REGISTRY);
   }
 
   private VESRegistry ()
